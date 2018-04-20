fae8ab2a0fd185abd719b0cf0827b1a6eef79ee6
Danny Stephens <danny.a.stephens@gmail.com>
2018-04-20 15:00:48 -0500

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,21 @@
+class minecraft {
+   file {'/opt/minecraft':
+     ensure => directory,
+   }
+   file {'/opt/minecraft/minecraft_server.jar':
+      ensure => file,
+      source => 'wget -O minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.7.4/minecraft_server.1.7.4.jar',
+   }
+   package {'java':
+     ensure => present,
+   }
+   file {'/opt/minecraft/eula.txt':
+     ensure => file,
+     content => 'eula=true',
+   }
+   file {'/etc/systemd/system/minecraft.service':
+     ensure => file,
+     source => 'puppet:///modules/minecraft/minecraft.service',
+   }
+
+}
